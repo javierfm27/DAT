4d3396fb3a6d2c4cf6183ae8d70457780b5ed0ca
javierfm27 <javitofm27@github.com>
2018-04-11 20:42:56 +0200

    Practica 4.9 Acabado

--- practica04/ccalculadora.js
+++ practica04/ccalculadora.js
@@ -6,129 +6,119 @@ login: jmorata
 'use strict'
 //Definimos la clase Calculadora
 class Calculadora{
-  
-}
-//States
-//State First Operating
-function firstOperating(key,calculator){
-  calculator.firstOperating = String(calculator.firstOperating) + key
-  calculator.firstOperating = Number(calculator.firstOperating)
-  console.log(calculator.firstOperating)
-}
-//Waiting Second Operating
-function waitingSecondOperating(calculator,key){
-  calculator.operator = key
-  calculator.haveOperator = true
-}
-
-//State Second Operating
-function secondOperating(key,calculator){
-  calculator.secondOperating = String(calculator.secondOperating) + key
-  calculator.secondOperating = Number(calculator.secondOperating)
-  console.log(calculator.secondOperating)
-}
-
-//State Show result
-function showResult(calculator){
-  let operator = String(calculator.operator)
-  switch (operator) {
-    case "+":
-      calculator.solution = calculator.firstOperating + calculator.secondOperating
-      console.log(calculator.solution)
-      break;
-    case "*":
-      calculator.solution = calculator.firstOperating * calculator.secondOperating
-      console.log(calculator.solution)
-      break;
-    case "-":
-      calculator.solution = calculator.firstOperating - calculator.secondOperating
-      console.log(calculator.solution)
-      break;
-    case "/":
-      calculator.solution = calculator.firstOperating / calculator.secondOperating
-      console.log(calculator.solution)
-      break;
+  constructor(firstOperating, secondOperating, operator, solution, haveOperator){
+    this.firstOperating = 0;
+    this.secondOperating = 0;
+    this.operator = NaN;
+    this.solution = 0;
+    this.haveOperator = false;
   }
-}
 
-//State
-function resetCalculator(calculator){
-  calculator.firstOperating = 0
-  calculator.secondOperating = 0
-  calculator.operator = NaN
-  calculator.haveOperator = false
-  calculator.solution = 0
-  console.log(0)
-}
+  //States
+  //State First Operating
+  primerOperando(key){
+    this.firstOperating = String(this.firstOperating) + key
+    this.firstOperating = Number(this.firstOperating)
+  }
+  //Waiting Second Operating
+  waitingSecondOperating(key){
+    this.operator = key
+    this.haveOperator = true
+  }
 
-//CheckOperator
-function checkOperator(key){
-  let operators = ["+","-","*","/"]
+  //State Second Operating
+  segundoOperando(key){
+    this.secondOperating = String(this.secondOperating) + key
+    this.secondOperating = Number(this.secondOperating)
+  }
 
-  for(let i of operators){
-    if(i==key){
-      return true
+  //State Show result
+  showResult(){
+    let operator = String(this.operator)
+    switch (operator) {
+      case "+":
+        this.solution = this.firstOperating + this.secondOperating
+        break;
+      case "*":
+        this.solution = this.firstOperating * this.secondOperating
+        break;
+      case "-":
+        this.solution = this.firstOperating - this.secondOperating
+        break;
+      case "/":
+        this.solution = this.firstOperating / this.secondOperating
+        break;
     }
   }
-  return false
-}
+
+  //State
+  resetCalculator(){
+    this.firstOperating = 0
+    this.secondOperating = 0
+    this.operator = NaN
+    this.haveOperator = false
+    this.solution = 0
+    return 0
+  }
+
+  //CheckOperator
+  checkOperator(key){
+    let operators = ["+","-","*","/"]
+
+    for(let i of operators){
+      if(i==key){
+        return true
+      }
+    }
+    return false
+  }
 
 
-function getKey(key, calc){
-  //Al iniciar la calculadora estaria mostrando 0, esperando el primer argumento. Pero de momento no vamos a programar esta
-  //funcionalidad, ya que todo se da seguido
+  getKey(key){
+    //Al iniciar la calculadora estaria mostrando 0, esperando el primer argumento. Pero de momento no vamos a programar esta
+    //funcionalidad, ya que todo se da seguido
 
-  //Check the able Key
-  if(!Number(key)){
-    if(checkOperator(key)){
-      if(calc.haveOperator == true){
-        showResult(calc)
-        calc.firstOperating = 0
-        firstOperating(String(calc.solution),calc)
-        calc.secondOperating = 0
-        waitingSecondOperating(calc,key)
+    //Check the able Key
+    if(!Number(key)){
+      if(this.checkOperator(key)){
+        if(this.haveOperator == true){
+          this.showResult(calc)
+          return(this.solution)
+          this.firstOperating = 0
+          this.primerOperando(String(this.solution))
+          this.secondOperating = 0
+          this.waitingSecondOperating(key)
+        }else{
+          this.waitingSecondOperating(key)
+        }
       }else{
-        waitingSecondOperating(calc,key)
+        if(key == "="){
+          this.showResult()
+          return(this.solution)
+        }else if (key == "c"){
+          this.resetCalculator()
+          return(solution)
+        }
       }
     }else{
-      if(key == "="){
-        showResult(calc)
-      }else if (key == "c"){
-        resetCalculator(calc)
+      if(this.haveOperator){
+        this.segundoOperando(key)
+        return(this.secondOperating)
+      }else{
+        this.primerOperando(key)
+        return(this.firstOperating)
       }
     }
-  }else{
-    if(calc.haveOperator){
-      secondOperating(key,calc)
-    }else{
-      firstOperating(key,calc)
-    }
   }
-}
 
 
-
-function testCalc(){
-  let calculator = {
-    firstOperating : 0,
-    secondOperating : 0,
-    operator : NaN,
-    solution : 0,
-    haveOperator: false,
-  }
-  getKey(3,calculator)
-  getKey("+",calculator)
-  getKey(5,calculator)
-  getKey("*",calculator)
-  getKey(2,calculator)
-  getKey("=",calculator)
-  getKey("c",calculator)
-  getKey(1,calculator)
-  getKey(1,calculator)
-  getKey("*",calculator)
-  getKey(2,calculator)
-  getKey("=",calculator)
 }
 
 //Main de JavaScript
-testCalc()
+//  Aqui probaremos los objetos
+let calc = new Calculadora();
+console.log(calc.getKey(3))
+console.log(calc.getKey(5))
+console.log(calc.getKey('+'))
+console.log(calc.getKey(4))
+console.log(calc.getKey('='))

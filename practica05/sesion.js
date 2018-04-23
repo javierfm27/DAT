008393501b3579c0092f3a3aab594f6e0309262e
javierfm27 <javitofm27@github.com>
2018-04-23 18:42:51 +0200

    Practica 5 Inacabada

--- practica05/sesion.js
+++ practica05/sesion.js
@@ -3,11 +3,35 @@ Login: jmorata
 Nombre: Javier Fernández Morata
  **/
 'use strict'
+let options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+}
+
 function initHtml(){
   let usuario = localStorage.getItem('user')
+  let fecha = new Date()
 
+  let message = "Hola " + usuario + ". Tu última visita fue con fecha " + fecha
   if (!usuario) {
     let p = prompt("Ingrese aqui su nombre", "p.e. Javier")
     localStorage.setItem('user',p)
+    let message = "Hola " + p + ". Esta es tu primera visita."
+    return message
   }
+  return message
+}
+
+function error(err){
+    console.warn('ERROR(${err.code}): ${err.message}')
+}
+
+function geoMessage(coord){
+  let x = coord.coords
+  return x
+}
+
+function remove(){
+  localStorage.clear()
 }

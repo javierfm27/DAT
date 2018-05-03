8e4fe24d38e0d53fdf0c79645a9e4cbdf07c9e20
javierfm27 <javitofm27@github.com>
2018-05-03 12:59:31 +0200

    Práctica 5.1 y 5.3 Mejoradas

--- practica05/sesion.js
+++ practica05/sesion.js
@@ -8,23 +8,15 @@ let options = {
   maximumAge: 0,
 }
 
-function initHtml(){
-  let usuario = localStorage.getItem('user')
-
-  if (!usuario) {
-    let p = prompt("Ingrese aqui su nombre", "p.e. Javier")
-    localStorage.setItem('user',p)
-  }
-}
 
 function success(position){
   let x = position.coords
 
   //Guardamos los datos en el diccionario
   console.log("Lo guardo")
-  localStorage.setItem("latitud",x.latitude)
-  localStorage.setItem("longitud",x.longitude)
-  localStorage.setItem("precision",x.accuracy)
+  localStorage.setItem("latitud",x.latitude.toFixed(2))
+  localStorage.setItem("longitud",x.longitude.toFixed(2))
+  localStorage.setItem("precision",x.accuracy.toFixed(2))
   localStorage.setItem('fecha', new Date())
 
 }
@@ -35,4 +27,15 @@ function error(err){
 
 function remove(){
   localStorage.clear()
+}
+
+function initObject(){
+  if(!(localStorage.getItem('user'))){
+
+    navigator.geolocation.getCurrentPosition(success, error, options)
+    let p = prompt("Ingrese aqui su nombre", "p.e. Javier")
+    localStorage.setItem('user',p)
+    return true
+  }
+  return false
 }
--- practica05/sesion2.js
+++ practica05/sesion2.js
@@ -5,31 +5,32 @@ Nombre: Javier Fernández Morata
 'use strict'
 let options = {
   enableHighAccuracy: true,
-  timeout: 5000,
   maximumAge: 0,
 }
 
-function initHtml(){
-  let usuario = localStorage.getItem('user')
-
-  if (!usuario) {
-    let p = prompt("Ingrese aqui su nombre", "p.e. Javier")
-    localStorage.setItem('user',p)
-  }
-}
-
-function obtainParamGeo(){
-    navigator.geolocation.getCurrentPosition(success, error, options)
+function initUser(){
+  let ubicacion = JSON.parse(localStorage.getItem('ubi'))
+  let p = prompt("Ingrese aqui su nombre", "p.e. Javier")
+  ubicacion.usuario = p
+  let ubiCadena = JSON.stringify(ubicacion)
+  localStorage.setItem('ubi',ubiCadena)
 }
 
 function success(position){
   let x = position.coords
 
-  //Generamos un objeto, y tras eso lo pasamos a Json y lo guardamos
-  let datos = [{'latitud':x.latitude},{'longitud':x.longitude},{'precision':x.accuracy}]
-  let cadenaJson = JSON.stringify(datos)
-  console.log(typeof(cadenaJson),cadenaJson)
-  localStorage.setItem('info',cadenaJson)
+  //Obtenemos la cadena JSON
+  let ubiObject = JSON.parse(localStorage.getItem('ubi'))
+
+  //Asignamos los valores
+  ubiObject.latitud = x.latitude.toFixed(2)
+  ubiObject.longitud = x.longitude.toFixed(2)
+  ubiObject.precision = x.accuracy.toFixed(2)
+  ubiObject.fecha = String(new Date())
+
+  //Guardamos la Cadena nueva
+  let ubiCadena = JSON.stringify(ubiObject)
+  localStorage.setItem('ubi',ubiCadena)
 }
 
 function error(err){
@@ -38,4 +39,25 @@ function error(err){
 
 function remove(){
   localStorage.clear()
+}
+
+function initObject(){
+  if(!(localStorage.getItem('ubi'))){
+    //Creamos un objeto llamado ubicacion, donde estaran todos los datos
+    let ubicacion = {
+      latitud : 0,
+      longitud: 0,
+      precision: 0,
+      fecha: 0,
+      usuario: 0,
+    }
+    localStorage.setItem('ubi',JSON.stringify(ubicacion))
+    navigator.geolocation.getCurrentPosition(success, error, options)
+    let p = prompt("Ingrese aqui su nombre", "p.e. Javier")
+    ubicacion = JSON.parse(localStorage.getItem('ubi'))
+    ubicacion.usuario= p
+    localStorage.setItem('ubi',JSON.stringify(ubicacion))
+    return true
+  }
+  return false
 }
